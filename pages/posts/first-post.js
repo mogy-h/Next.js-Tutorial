8ea61dd2e552b51f162c123b36a907b3103c7c32
mogy-h <rapter.mg@gmail.com>
2021-09-13 21:40:47 +0900

    Headタグ内部を変更

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post by mogy-h</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-      <img src="/images/profile.jpg" alt="Your Name" />
     </>
   );
 }
