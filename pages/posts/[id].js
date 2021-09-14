d324768592df22786c6d7c65717ac73d0bf0b05f
mogy-h <rapter.mg@gmail.com>
2021-09-14 22:33:32 +0900

    Dateコンポーネントで日付を表示

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -35,7 +36,7 @@ export default function Post({ postData }) {
       <br />
       {postData.id}
       <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
